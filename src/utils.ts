844c6b27a0dde028a1052e03d1f4482709448ae3
yoshimitsu198 <yoshimitsu198@users.noreply.github.com>
2025-12-23 07:44:19 +0200

    style: add unit tests

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+// Updated iteration 3
+function func3(): boolean {
+    return true;
+}
+
+function processData3(data: string): string | null {
+    if (data) {
+        return data.toUpperCase();
+    }
+    return null;
+}
